055356f0d67c951d79240e91ffd500c21ea02f86
v.bianchi <valerio.bianchi@euforlegal.it>
2022-09-01 20:22:06 +0200

    first commit

---
+++ src/api.js
@@ -0,0 +1,24 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'a50a9c68ecmshcfc8a28079ad4c6p1a14b5jsnc5d93f6ce30a',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "09c1d445f2a11d37ecd89bc2500a397b";
+
+export const FORECAST_LIMIT_NUMBER = 7;
+
+export const WEEK_DAYS = [
+	'Monday',
+	'Tuesday',
+	'Wednesday',
+	'Thursday',
+	'Friday',
+	'Saturday',
+	'Sunday'
+];
